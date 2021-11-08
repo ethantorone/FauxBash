9dbcc927d2f0ef6cabca10706bba7efd3580ad82
Ethan Torone <et03679@uga.edu>
2021-11-07 22:29:44 -0500

    about to comment out entirety of code lol

--- Torone-Ethan-p2/head.c
+++ Torone-Ethan-p2/head.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 #define BUFF_SIZE 1024
 
@@ -10,8 +11,12 @@ int main(int argc, char* argv[]) {
     int numLines = 10;
     char c = 0;
     int numBytes = 0;
+
     char * filename = argv[argc - 1];
-    int fileDescriptor = open(filename, 0_RDONLY, 0644);
+    int file = open(filename, O_RDONLY, 0644);
+    if (file == -1) {
+        file = STDIN_FILENO;
+    }
 
     char buffer[BUFF_SIZE];
 
@@ -28,13 +33,27 @@ int main(int argc, char* argv[]) {
         } //if
     } //for
 
-
+    int rres;
+    int wres = 0;
+    int buffLength;
 
     if (n == 1) {
-        for (int i = 1; i <= numLines | ; i++ ) {
 
+        for (int i = 0; (i < numLines) & (wres <= buffLength); i++) {
+
+            read(file, buffer, BUFF_SIZE);
+            buffLength = strlen(buffer);
+            wres = 0;
+            while (wres < buffLength) {
+                wres += write(STDOUT_FILENO, buffer + wres, 1);
+                if (!(buffer[wres] == '\n')) {
+                    i--;
+                }
+            }
         }
     } else if (c == 1) {
+        read(file, buffer, BUFF_SIZE);
+        buffLength = strlen(buffer);
         for (int i = 1; i <= numBytes; i++) {
 
         }
